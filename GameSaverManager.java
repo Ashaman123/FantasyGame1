eb82c76e9bba7c791bdc32f5863a7d984466e03b
Ashaman123 <odogb02@gmail.com>
2023-09-26 08:21:54 -0600

    Add files via upload

---
+++ GameSaverManager.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class GameSaverManager {
+    public static void saveGameState(GameState gameState, String fileName) {
+        // Saving logic here
+    	
+    }
+
+    public static GameState loadGameState(String fileName) {
+        // Loading logic here
+        return null; // Replace with actual loaded game state
+    }
+}
---
+++ GameState.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+
+public class GameState implements Serializable {
+    // Define variables to store game data
+    private int playerScore;
+    private String playerName;
+    // Add other game-related data here
+
+    // Create getters and setters for the variables
+    // ...
+    public void setPlayerScore() {
+    	int PlayerScore = 100;
+    }
+    public void setPlayerName() {
+    	String PlayerName = "Player1";
+    }
+}
