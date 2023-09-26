fcea3b0fd268cf289f25d3c751e32fcf1df2789a
Ashaman123 <odogb02@gmail.com>
2023-09-26 16:57:03 -0600

    Commit

--- Enemy.java
+++ Enemy.java
@@ -4,20 +4,36 @@ public class Enemy {
 	private Random random = new Random();
 
 	private int level; 
-	private int maxHp;
-	private int hp;
-	private int attackDamage;
-	private int defense;
-	
+	private double maxHp;
+	private double hp;
+	private double attackDamage;
+	private int armor;
+	private int experienceGiven;
 	
 	public Enemy() {
+			this.experienceGiven = 15;
 	        this.level = 1;
 	        this.maxHp = 100;
 	        this.hp = 100;
 	        this.attackDamage = 10;
-	        this.defense = 10;
+	        this.armor = 5;
 	}
 	 
 
+	public void takeDamage(double damageDealt) {
+	    double damageBlocked = Math.max(0, armor);
+	    this.hp -= damageDealt - damageBlocked;
+	    
+	    if (this.hp < 0) {
+	        this.hp = 0;
+	        enemyDefeated();
+	    }
+	}
+
+
+	public void enemyDefeated() {
+		Character.addExperince(this.experienceGiven);
+	}
+	
 }
 
